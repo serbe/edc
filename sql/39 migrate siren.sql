0b841f64f5591f080d0ed9047681040bdc641840
Valeriy Kostikov <mkoctuk@gmail.com>
2017-07-03 21:26:00 +0300

    fix sql

--- sql/39 migrate siren.sql
+++ sql/39 migrate siren.sql
@@ -1,13 +1,1 @@
-﻿SELECT
-		e.id,
-		e.start_date,
-		c.name
-	FROM
-		educations AS e
-	LEFT JOIN
-		contacts AS c ON c.id = e.contact_id
-	WHERE
-		e.start_date > TIMESTAMP 'now'::timestamp - '1 month'::interval
-	ORDER BY
-		start_date DESC
-	LIMIT 10
+﻿ALTER TABLE sirens OWNER TO eddsuser;
