c0ab23e5babd388b990993cf3a117eb3fd56cff4
Valeriy Kostikov <mkoctuk@gmail.com>
2019-12-18 07:39:18 +0300

    finish migrate

--- phone.go
+++ phone.go
@@ -1,5 +1,10 @@
 package edc
 
+import (
+	"context"
+	"time"
+)
+
 // Phone - struct for phone
 type Phone struct {
 	ID        int64  `sql:"id"            json:"id"           form:"id"         query:"id"`
@@ -7,186 +12,115 @@ type Phone struct {
 	ContactID int64  `sql:"contact_id,pk" json:"contact_id"   form:"contact_id" query:"contact_id"`
 	Phone     int64  `sql:"phone"         json:"phone,string" form:"phone"      query:"phone"`
 	Fax       bool   `sql:"fax"           json:"fax"          form:"fax"        query:"fax"`
- 	CreatedAt string `sql:"created_at"    json:"-"`
- 	UpdatedAt string `sql:"updated_at"    json:"-"`
+	CreatedAt string `sql:"created_at"    json:"-"`
+	UpdatedAt string `sql:"updated_at"    json:"-"`
 }
-
-// // PhoneGet - get one phone by id
-// func PhoneGet(id int64) (Phone, error) {
-// 	var phone Phone
-// 	if id == 0 {
-// 		return phone, nil
-// 	}
-// 	err := pool.QueryRow(context.Background(), &phone).
-// 		Where("id = ?", id).
-// 		Select()
-// 	if err != nil {
-// 		errmsg("GetPhone select", err)
-// 	}
-// 	return phone, nil
-// }
-
-// // PhoneListGet - get all phones for list
-// func PhoneListGet() ([]Phone, error) {
-// 	var phones []Phone
-// 	err := pool.QueryRow(context.Background(), &phones).
-// 		Column("id", "company_id", "contact_id", "phone", "fax").
-// 		Order("phone ASC").
-// 		Select()
-// 	if err != nil {
-// 		errmsg("GetPhoneList select", err)
-// 	}
-// 	return phones, err
-// }
-
-// // CompanyPhonesGet - get all phones by company id
-// func CompanyPhonesGet(id int64, fax bool) ([]Phone, error) {
-// 	var phones []Phone
-// 	if id == 0 {
-// 		return phones, nil
-// 	}
-// 	err := pool.QueryRow(context.Background(), &phones).
-// 		Where("company_id = ? AND fax = ?", id, fax).
-// 		Order("phone ASC").
-// 		Select()
-// 	if err != nil {
-// 		errmsg("GetCompanyPhones select", err)
-// 	}
-// 	return phones, err
-// }
-
-// // ContactPhonesGet - get all phones by contact id
-// func ContactPhonesGet(id int64, fax bool) ([]Phone, error) {
-// 	var phones []Phone
-// 	if id == 0 {
-// 		return phones, nil
-// 	}
-// 	err := pool.QueryRow(context.Background(), &phones).
-// 		Where("contact_id = ? AND fax = ?", id, fax).
-// 		Order("phone ASC").
-// 		Select()
-// 	if err != nil {
-// 		errmsg("GetContactPhones select", err)
-// 	}
-// 	return phones, nil
-// }
 
 // PhoneInsert - create new phone
 func PhoneInsert(phone Phone) (int64, error) {
 	phone.ID = 0
-	err := pool.Insert(&phone)
+	err := pool.QueryRow(context.Background(), `
+		INSERT INTO phones
+		(
+			company_id,
+			contact_id,
+			phone,
+			fax,
+			created_at,
+			updated_at
+		)
+		VALUES
+		(
+			$1,
+			$2,
+			$3,
+			$4,
+			$5,
+			$6
+		)
+	`, phone.CompanyID, phone.ContactID, phone.Phone, phone.Fax, time.Now(), time.Now()).Scan(&phone.ID)
 	if err != nil {
-		errmsg("CreatePhone insert", err)
+		errmsg("PhoneInsert QueryRow", err)
 	}
 	return phone.ID, nil
 }
 
-// CompanyPhonesUpdate - update company phones
-func CompanyPhonesUpdate(company Company, fax bool) error {
-	err := e.DeleteCompanyPhones(company.ID, fax)
+// PhonesCompanyUpdate - update company phones
+func PhonesCompanyUpdate(id int64, phones []int64, fax bool) error {
+	err := PhonesCompanyDelete(id, fax)
 	if err != nil {
-		errmsg("UpdateCompanyPhones DeleteCompanyPhones", err)
+		errmsg("PhonesCompanyUpdate PhonesCompanyDelete", err)
 		return err
 	}
-	var allPhones []int64
-	if fax {
-		allPhones = company.Faxes
-	} else {
-		allPhones = company.Phones
-	}
-	for i := range allPhones {
+	for i := range phones {
 		var phone Phone
-		phone.CompanyID = company.ID
-		phone.Phone = allPhones[i]
+		phone.CompanyID = id
+		phone.Phone = phones[i]
 		phone.Fax = fax
-		_, err = e.CreatePhone(phone)
+		_, err = PhoneInsert(phone)
 		if err != nil {
-			errmsg("UpdateCompanyPhones CreatePhone", err)
+			errmsg("PhonesCompanyUpdate PhoneInsert", err)
 			return err
 		}
 	}
 	return nil
 }
 
-// ContactPhonesUpdate - update contact phones
-func ContactPhonesUpdate(contact Contact, fax bool) error {
-	err := e.DeleteContactPhones(contact.ID, fax)
+// PhonesContactUpdate - update contact phones
+func PhonesContactUpdate(id int64, phones []int64, fax bool) error {
+	err := PhonesContactDelete(id, fax)
 	if err != nil {
-		errmsg("CreateContactPhones DeleteContactPhones", err)
+		errmsg("PhonesContactUpdate PhonesContactDelete", err)
 		return err
 	}
-	var allPhones []int64
-	if fax {
-		allPhones = contact.Faxes
-	} else {
-		allPhones = contact.Phones
-	}
-	for i := range allPhones {
+	for i := range phones {
 		var phone Phone
-		phone.ContactID = contact.ID
-		phone.Phone = allPhones[i]
+		phone.ContactID = id
+		phone.Phone = phones[i]
 		phone.Fax = fax
-		_, err = e.CreatePhone(phone)
+		_, err = PhoneInsert(phone)
 		if err != nil {
-			errmsg("UpdateContactPhones CreatePhone", err)
+			errmsg("PhonesContactUpdate PhoneInsert", err)
 			return err
 		}
 	}
 	return nil
 }
 
-// CompanyPhonesDelete - delete all unnecessary phones by company id
-func CompanyPhonesDelete(id int64, fax bool) error {
+// PhonesCompanyDelete - delete all unnecessary phones by company id
+func PhonesCompanyDelete(id int64, fax bool) error {
 	if id == 0 {
 		return nil
 	}
-	_, err := pool.QueryRow(context.Background(), &Phone{}).
-		Where("company_id = ? and fax = ?", id, fax).
-		Delete()
+	_, err := pool.Exec(context.Background(), `
+		DELETE FROM
+			phones
+		WHERE
+			company_id = $1
+		AND
+			fax = $2
+	`, id, fax)
 	if err != nil {
-		errmsg("DeleteCompanyPhones delete", err)
+		errmsg("PhonesCompanyDelete Exec", err)
 	}
 	return err
 }
 
-// ContactPhonesDelete - delete all unnecessary phones by contact id
-func ContactPhonesDelete(id int64, fax bool) error {
+// PhonesContactDelete - delete all unnecessary phones by contact id
+func PhonesContactDelete(id int64, fax bool) error {
 	if id == 0 {
 		return nil
 	}
-	_, err := pool.QueryRow(context.Background(), &Phone{}).
-		Where("contact_id = ? and fax = ?", id, fax).
-		Delete()
+	_, err := pool.Exec(context.Background(), `
+		DELETE FROM
+			phones
+		WHERE
+			contact_id = $1
+		AND
+			fax = $2
+	`, id, fax)
 	if err != nil {
-		errmsg("DeleteContactPhones delete", err)
-	}
-	return err
-}
-
-// AllCompanyPhonesDelete - delete all phones and faxes by company id
-func AllCompanyPhonesDelete(id int64) error {
-	if id == 0 {
-		return nil
-	}
-	_, err := pool.QueryRow(context.Background(), &Phone{}).
-		Where("company_id = ?", id).
-		Delete()
-	if err != nil {
-		errmsg("DeleteAllCompanyPhones delete", err)
-	}
-	return err
-}
-
-// AllContactPhonesDelete - delete all phones and faxes by contact id
-func AllContactPhonesDelete(id int64) error {
-	if id == 0 {
-		return nil
-	}
-	_, err := pool.QueryRow(context.Background(), &Phone{}).
-		Where("contact_id = ?", id).
-		Delete()
-	if err != nil {
-		errmsg("DeleteAllContactPhones delete", err)
+		errmsg("PhonesContactDelete Exec", err)
 	}
 	return err
 }
@@ -201,13 +135,12 @@ func phoneCreateTable() error {
 				phone bigint,
 				fax bool NOT NULL DEFAULT false,
 				created_at TIMESTAMP without time zone,
-				updated_at
- TIMESTAMP without time zone default now()
+				updated_at TIMESTAMP without time zone default now()
 			)
 	`
-	_, err := pool.Exec(str)
+	_, err := pool.Exec(context.Background(), str)
 	if err != nil {
-		errmsg("phoneCreateTable exec", err)
+		errmsg("phoneCreateTable Exec", err)
 	}
 	return err
 }
